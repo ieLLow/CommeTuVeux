a2dfc0e60589e99d690ba3c4ea3ffbeedb5d9fdf
Shaun Badou <shaunbadou@outlook.be>
2018-04-10 17:17:02 +0200

    Start RetryMenu

---
+++ Assets/Scripts/RetryMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RetryMenu : MonoBehaviour
+{
+
+    public bool isDead = false;
+    public GameObject retryMenu;
+    public float timeLeft = 5f;
+
+    void Start ()
+    {
+        
+
+    }
+
+    void Update ()
+    {
+        if (isDead == true)
+        {
+            retryMenu.SetActive(true);
+            timeLeft -= Time.deltaTime;
+            if(timeLeft < 0)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+        else
+        {
+            retryMenu.SetActive(false);
+        }
+    }
+}
